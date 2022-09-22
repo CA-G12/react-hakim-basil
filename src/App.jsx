938dd0a53b0309ea5ec73c6251595267fc3be08a
Hakim <abdalhakem1226@gmail.com>
2022-09-22 11:24:20 +0300

    filter property is done Relates #12

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
+import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import LandingSection from "./components/LandingSection";
 import CardsContainer from "./components/CardsContainer";
 
-function App() {
-  return (
-    <>
-      <Header />
-      <LandingSection />
-      <div className="div">
-        <h2 className="animeSectionTitle">Animes List</h2>
-        <CardsContainer />
-      </div>
-    </>
-  );
+class App extends React.Component {
+  state = {
+    search: "",
+  };
+
+  handleSearch = (value) => {
+    this.setState({ search: value });
+  };
+
+  render() {
+    return (
+      <>
+        <Header serach={this.state.search} handleSearch={this.handleSearch} />
+        <LandingSection />
+        <div className="div">
+          <h2 className="animeSectionTitle">Animes List</h2>
+          <CardsContainer search={this.state.search} />
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
